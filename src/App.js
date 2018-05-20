0a077f3fc176d9e04a75648814811f1e81a0a9bc
crea <654321su@gmail.com>
2018-05-20 16:20:04 +0900

    #2-3 Lists with .map

--- src/App.js
+++ src/App.js
@@ -2,30 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-];
-
-const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
-  "https://20ui41tp7v127j03rcnp97oh-wpengine.netdna-ssl.com/wp-content/uploads/2018/01/alberto_fullmetal.jpg",
-  "https://resizing.flixster.com/V1wwb_8cVenJCCUQ43kYYhYQoMA=/206x305/v1.bTsxMTE3NjY0NjtqOzE3NzY5OzEyMDA7MTQwMDsyMTAw",
-  "https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
+const movies = [
+  {
+    title: "matrix",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://20ui41tp7v127j03rcnp97oh-wpengine.netdna-ssl.com/wp-content/uploads/2018/01/alberto_fullmetal.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://resizing.flixster.com/V1wwb_8cVenJCCUQ43kYYhYQoMA=/206x305/v1.bTsxMTE3NjY0NjtqOzE3NzY5OzEyMDA7MTQwMDsyMTAw"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
+  } 
 ];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Movie title={movieTitles[0]} poster={movieImages[0]} />
-       <Movie title={movieTitles[1]} poster={movieImages[1]} />
-       <Movie title={movieTitles[2]} poster={movieImages[2]} />
-       <Movie title={movieTitles[3]} poster={movieImages[3]} />
+       {movies.map(movie => {
+         return <Movie title={movie.title} poster={movie.poster} />
+       })}
       </div>
-    )
+    );
   }
 }
 
